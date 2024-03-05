7f699a61bc2fdb9dd40019844496a9b1209f16d4
Cesar Nsingi <cesarricardo1805@hotmail.com>
2024-03-06 02:30:42 +0400

    add task fixed

--- main.js
+++ main.js
@@ -36,17 +36,18 @@ form.addEventListener('submit', (e) => {
     task_edit_el.classList.add("edit");
     task_edit_el.innerHTML = "Edit";
 
-    const task_delete_el = document.createElement("button");
+    const task_delete_el = document.createElement
+    ("button");
     task_delete_el.classList.add("delete");
     task_delete_el.innerHTML = "Delete";
     
     task_actions_el.appendChild(task_edit_el);
     task_actions_el.appendChild(task_delete_el);
 
-    task_actions_el.appendChild(task_actions_el);
+    task_el.appendChild(task_actions_el);
 
     list_el.appendChild(task_el);
-    
+
     input.value = "";
 
 })
