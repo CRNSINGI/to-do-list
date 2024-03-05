274b2747d61581c5c2c4b1fd4fbe6dfff9e1613d
Cesar Nsingi <cesarricardo1805@hotmail.com>
2024-03-06 03:16:57 +0400

    js property debug

--- main.js
+++ main.js
@@ -50,7 +50,23 @@ form.addEventListener('submit', (e) => {
 
     input.value = "";
 
-})
+    task_edit_el.addEventListener('click', () => {
+        if (task_edit_el.innerText.toLowerCase() == 
+        "edit") {
+            task_input_el.removeAttribute("readonly");
+            task_input_el.focus();
+            task_edit_el.innerText = "Save";
 
-})
+        } else { 
+            task_input_el.setAttribute("readonly",
+            "readonly");
+            task_edit_el.innerText = "Edit";
+        }
+    });
+    task_delete_el.addEventListener('click', () => {
+        list_el.removeChild(task_el);
+    });
+});
 
+});
+ 
